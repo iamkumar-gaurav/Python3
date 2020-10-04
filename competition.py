a87b8b9ccdcc11f729dafec794f660b963a05657
Kumar Gaurav <iamkumar_gaurav@hotmail.com>
2020-10-04 22:27:46 +0530

    Add files via upload

---
+++ competition.py
@@ -0,0 +1,17 @@
+
+
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def fizzBuzz(n):
+    for i in range(n):
+        print(i)
+    # Write your code here
+
+if __name__ == '__main__':
+    n=input()
---
+++ hello.py
@@ -0,0 +1,18 @@
+var1=70.02;
+print(var1+20);
+var=20;
+print(var+20);
+txt="he said :\"yo should get the same results.\"";
+print(txt);
+print(txt[0]);
+print(txt[len(txt)-5]);
+bith_age = input("Enter your D.O.B:-")
+age=2019-int(bith_age)
+print(age);
+
+name = input("enter your age")
+kg = float(name)/1000
+print(kg);
+print(type(var1));
+
+
